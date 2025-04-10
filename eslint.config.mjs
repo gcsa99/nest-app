32c8a74424cb6c0a8b9a87bc71d4e847bb7581d0
root <root@Desktop-Gui.>
2025-04-10 12:01:22 -0300

    feat: add husky and lint configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,3 @@
-// @ts-check
 import eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
@@ -6,12 +5,27 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['eslint.config.mjs', 'dist/**', 'node_modules/**'],
+    ignores: [
+      'eslint.config.mjs',
+      '**/node_modules/**',
+      '.husky/**',
+      'dist/**',
+    ],
     files: ['**/*.ts', '**/*.js'],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
   eslintPluginPrettierRecommended,
+  {
+    files: ['**/*.ts', '**/*.tsx'], // Apply projectService only to TS/TSX files
+    languageOptions: {
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: import.meta.dirname,
+        projectService: true,
+      },
+    },
+  },
   {
     languageOptions: {
       globals: {
@@ -20,17 +34,14 @@ export default tseslint.config(
       },
       ecmaVersion: 5,
       sourceType: 'module',
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
     },
   },
   {
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-floating-promises': 'off',
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      'quotes': ['error', 'single'],
     },
   },
 );
