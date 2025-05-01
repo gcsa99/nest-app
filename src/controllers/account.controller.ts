47c6214c43365ed0584fe12e03e86987d06e3b60
root <root@Desktop-Gui.>
2025-05-01 14:55:08 -0300

    refact: using one controller for all account endpoints

--- src/controllers/account.controller.ts
+++ src/controllers/account.controller.ts
@@ -8,26 +8,18 @@ import {
   Post,
 } from '@nestjs/common';
 import { hash } from 'bcryptjs';
-import { ZodValidationPipe } from 'src/pipes/zod-validation.pipe';
+import { CreateAccountBodySchema, createAccountValidationPipe } from 'src/dto';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { z } from 'zod';
-
-const createAccountBodySchema = z.object({
-  name: z.string(),
-  email: z.string().email(),
-  password: z.string(),
-});
-type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>;
 
 @Controller('accounts')
-export class CreateAccountController {
+export class AccountController {
   constructor(private readonly prisma: PrismaService) {}
   logger: LoggerService = new Logger(this.constructor.name);
 
   @Post()
   @HttpCode(201)
   async handle(
-    @Body(new ZodValidationPipe(createAccountBodySchema))
+    @Body(createAccountValidationPipe)
     data: CreateAccountBodySchema,
   ) {
     this.logger.log('Creating new account');
---
+++ src/dto/account/create-account.dto.ts
@@ -0,0 +1,13 @@
+import { ZodValidationPipe } from 'src/pipes/zod-validation.pipe';
+import { z } from 'zod';
+
+export const createAccountBodySchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string(),
+});
+export type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>;
+
+export const createAccountValidationPipe = new ZodValidationPipe(
+  createAccountBodySchema,
+);
