e13e330e2a5b1ac41ca6dcf72275d5370100cc28
root <root@Desktop-Gui.>
2025-05-01 14:54:06 -0300

    chore: config env and add packets

--- config/env.ts
+++ config/env.ts
@@ -7,6 +7,8 @@ export const envSchema = z.object({
   URL: z.string().url().optional().default('http://localhost'),
   PORT: z.coerce.number().optional().default(3333),
   DATABASE_URL: z.string().url(),
+  JWT_PUBLIC: z.string().min(10),
+  JWT_SECRET: z.string().min(10),
 });
 
 export type Env = z.infer<typeof envSchema>;
