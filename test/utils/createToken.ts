0e2d7efca83547642887d69f6bc1b1fa9a5594e4
root <root@Desktop-Gui.>
2025-05-07 22:13:59 -0300

    feat: import core and domain from another project

--- test/utils/createToken.ts
+++ test/utils/createToken.ts
@@ -1,4 +1,4 @@
-import { PrismaService } from '@/prisma/prisma.service';
+import { PrismaService } from '@/infra/database/prisma/prisma.service';
 import { JwtService } from '@nestjs/jwt';
 import { TestingModule } from '@nestjs/testing';
 import { createAccount } from '@test/shared';
